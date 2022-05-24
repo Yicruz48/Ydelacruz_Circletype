75d8100b8776a20eb3374b56ce5a10701dae31bb
Yicruz48 <85276537+Yicruz48@users.noreply.github.com>
2022-05-23 20:38:45 -0700

    Exported code from codepen

---
+++ script.js
@@ -0,0 +1,6 @@
+import CircleType from "https://cdn.skypack.dev/circletype@2.3.0";
+
+// Instantiate `CircleType` with an HTML element.
+const circleType = new CircleType(document.getElementById('circle'));
+
+
